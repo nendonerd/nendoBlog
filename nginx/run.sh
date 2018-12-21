be77248b698a5d20894888b3d4c6668f98f7b832
nendonerd <wang3721.2011@gmail.com>
2018-12-22 01:07:48 +0800

    debugging

--- nginx/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "################################## Run nginx ##############################"
-export DOLLAR='$'
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf # /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
