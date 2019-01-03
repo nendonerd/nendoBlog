147ff681d4ccd44c03195e1a5d46501de4b83cd2
nendonerd <wang3721.2011@gmail.com>
2019-01-03 20:13:58 +0800

    add some tasks

---
+++ tasks/sync.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rsync -a root@193.110.202.159:/root/nendoBlog/ghost/content/ /Users/jack/Desktop/CS/projects/nendoBlog/ghost/content
+rsync -a root@193.110.202.159:/root/nendoBlog/isso/db/ /Users/jack/Desktop/CS/projects/nendoBlog/isso/db
