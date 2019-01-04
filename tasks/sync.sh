28bc05399739ef9af0a69f629e6595280a0d2c81
nendonerd <wang3721.2011@gmail.com>
2019-01-04 21:25:16 +0800

    update tasks

--- tasks/sync.sh
+++ tasks/sync.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 rsync -a root@193.110.202.159:/root/nendoBlog/ghost/content/ /Users/jack/Desktop/CS/projects/nendoBlog/ghost/content
 rsync -a root@193.110.202.159:/root/nendoBlog/isso/db/ /Users/jack/Desktop/CS/projects/nendoBlog/isso/db
+rsync -a /Users/jack/Desktop/CS/projects/nendoBlog/.env root@193.110.202.159:/root/nendoBlog/.env
