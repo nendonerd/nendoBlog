9d7a997751b47278f69b4fdf82a48eefc7c4fd48
nendonerd <wang3721.2011@gmail.com>
2019-03-25 11:21:38 +0800

    dynamically load isso

--- tasks/sync.sh
+++ tasks/sync.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-rsync -a root@193.110.202.159:/root/nendoBlog/ghost/content/ /Users/jack/Desktop/CS/projects/nendoBlog/ghost/content
-rsync -a root@193.110.202.159:/root/nendoBlog/isso/db/ /Users/jack/Desktop/CS/projects/nendoBlog/isso/db
-rsync -a /Users/jack/Desktop/CS/projects/nendoBlog/.env root@193.110.202.159:/root/nendoBlog/.env
+rsync -a root@193.110.202.159:/home/wang3721_2011/nendoBlog/ghost/content/ /Users/jack/Desktop/CS/projects/nendoBlog/ghost/content
+rsync -a root@193.110.202.159:/home/wang3721_2011/nendoBlog/isso/db/ /Users/jack/Desktop/CS/projects/nendoBlog/isso/db
+rsync -a /Users/jack/Desktop/CS/projects/nendoBlog/.env root@193.110.202.159:/home/wang3721_2011/nendoBlog/.env
+
+# rsync -a /Users/jack/Desktop/CS/projects/nendoBlog/ghost/content/ wang3721_2011@34.92.170.49:/home/wang3721_2011/nendoBlog/ghost/content
+# rsync -a /Users/jack/Desktop/CS/projects/nendoBlog/isso/db/ wang3721_2011@34.92.170.49:/home/wang3721_2011/nendoBlog//isso/db
